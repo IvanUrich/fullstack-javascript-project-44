679f985a189c8ba7aa4efa0d7c12d2e92063d4c0
Ivan Urich <151027949+IvanUrich@users.noreply.github.com>
2024-02-17 22:53:01 +0000

    InterIntermediate commit Step4-5

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,3 +4,4 @@ import { whatIsYourName } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 whatIsYourName();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
