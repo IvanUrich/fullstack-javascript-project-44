d66760108ccd8254158bbf566a1a219ac536b23e
Ivan Urich <151027949+IvanUrich@users.noreply.github.com>
2024-02-17 20:11:43 +0000

    Step-3 Third party libraries

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from "readline-sync";
 
-const readlineSync = require('readline-sync');
-const name = readlineSync.question('What is your name? ');
+export const whatIsYourName = () => {
+const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
+};
