efb006c344f73b94a7681202e001854e49594276
IvanUrich <piu-77@yandex.ru>
2024-02-19 18:43:34 +0300

    fix -> ESlint

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable no-eval */
 
 import { questionAnswer, checkingTheAnswer } from '../src/index.js'; // Импорт [ Import ]
 import { whatIsYourName } from '../src/cli.js';
@@ -7,8 +8,9 @@ const name = whatIsYourName();
 console.log('What is the result of the expression?'); // Правила игры [ Rules of the game ]
 const operator = ['+', '-', '*'];
 
-for (let i = 0; i < 3; i +=  1) { // Цикл на три вопроса  [ A series of three questions ]
+// Цикл на три вопроса  [ A series of three questions ]
 
+for (let i = 0; i < 3; i += 1) {
   const numberFirst = Math.floor(Math.random() * 100); // Random number from 1 to 100
   const numberSecond = Math.floor(Math.random() * 100); // Random number from 1 to 100
   const indexOperand = Math.abs(Math.floor((Math.random() * 10) / 3 - 1)); // Operator index
@@ -16,10 +18,10 @@ for (let i = 0; i < 3; i +=  1) { // Цикл на три вопроса  [ A se
   const answer = Number(questionAnswer(`${numberFirst} ${operator[indexOperand]} ${numberSecond}`)); // Asked question + answer
   const examination = eval(`${numberFirst} ${operator[indexOperand]} ${numberSecond}`); // Test answer
 
-  if (checkingTheAnswer(answer, examination) === false) { // Проверка на провал и победу [ Check for failure and victory ]
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${examination}'.`)
+  if (checkingTheAnswer(answer, examination) === false) { // Check for failure and victory
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${examination}'.`);
     console.log(`Let's try again, ${name}!`);
-    break; 
+    break;
   } else if (i === 2) {
     console.log(`Congratulations, ${name}!`);
   }
