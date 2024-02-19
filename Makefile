3a06611314991cda2b9c586978042cf4ad21b89c
IvanUrich <piu-77@yandex.ru>
2024-02-19 16:19:41 +0300

    fix brain-even

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ brain-games:
 		node bin/brain-games.js
 
 brain-even:
-		node bin/games/brain-even.js
+		node bin/brain-even.js
 
 brain-calc:
 		node bin/games/brain-calc.js
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
-#!/usr/bin/games/env node
+#!/usr/bin/env node
 
 import { questionAnswer } from '../src/index.js';  // Импорт [ Import ]
 import { whatIsYourName } from '../src/cli.js'; 
